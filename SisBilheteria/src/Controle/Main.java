8a94874db01d4b58232026a279e39ec143f82270
Rafael Madaloz <rafaelmadaloz@outlook.com>
2017-11-29 20:59:06 -0200

    ultima

--- SisBilheteria/src/Controle/Main.java
+++ SisBilheteria/src/Controle/Main.java
@@ -15,25 +15,6 @@ public class Main {
 
 		TelaInicial tela = new TelaInicial(data);
 
-
-		/*MenuPrincipal menu = new MenuPrincipal();
-		menu.menuInicio();
-		/*Database data = new Database();
-		MenuComprarIngresso menui = new MenuComprarIngresso(data);
-		Filme filme1 = menui.selecionarFilme(1);
-		System.out.println(filme1.poltronasDisponiveis());
-		Filme filme2 = menui.selecionarFilme(2);
-		System.out.println(filme2.poltronasDisponiveis());
-		System.out.println("comprando poltrona 1 filme1");
-		filme1.comprarPoltrona(1);
-		System.out.println(filme1.poltronasDisponiveis());
-		filme2.comprarPoltrona(2);
-		System.out.println("comprando poltrona 2 filme2");
-		System.out.println(filme2.poltronasDisponiveis());
-		*/
-
-
-
 	}
 
 
